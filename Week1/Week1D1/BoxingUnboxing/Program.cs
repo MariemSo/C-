0e36507e551f1bdb165fd778b9cce681da95adc4
MariemSo <souayah.mariem@gmail.com>
2023-10-27 16:31:20 +0200

    Boxing Unboxing

---
+++ Week1/Week1D1/BoxingUnboxing/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("-------Boxing / Unboxing-------");
+
+// Create an empty List of type object
+List <object>  values = new List<object>();
+// Add the following values to the list: 7, 28, -1, true, "chair"
+values.Add(7);
+values.Add(28);
+values.Add(-1);
+values.Add(true);
+values.Add("chair");
+
+int sum=0;
+// Loop through the list and print all values
+foreach (object item in values)
+{
+    Console.WriteLine("these are all items in the list: " +item);
+}
+Console.WriteLine("-------------------------------");
+
+foreach (object item in values){   
+    // Add all values that are Int type together and output the sum
+    if (item is int){
+        Console.WriteLine("this is an int: "+item);
+        int x = Convert.ToInt32(item);
+        sum += x;
+    }
+}
+Console.WriteLine("-------------------------------");
+Console.WriteLine("this is the Sum of all int:"+sum);
+
+
+
+
