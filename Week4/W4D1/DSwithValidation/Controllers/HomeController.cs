1ca1473d1e587b52c4ee3e9230983756344e403c
Mariem.S <souayah.mariem@gmail.com>
2023-11-21 12:13:12 +0100

    DS with Validation

---
+++ Week4/W4D1/DSwithValidation/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DSwithValidation.Models;
+
+namespace DSwithValidation.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("process")]
+    public IActionResult Create(Survey newSurvey)
+    { 
+        if (ModelState.IsValid)
+        {
+            System.Console.WriteLine($"{newSurvey.Name}");
+            return View("Result", newSurvey);
+        }
+        return View("Index");
+    }
+
+    [HttpGet("result")]
+    public IActionResult Result()
+    {
+        return View();
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
